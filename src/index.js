99c6e2802c773c13704a340e329a54b9ce14d6ac
Evgeniya <eva.gribova.2003@gmail.com>
2024-08-15 23:32:07 +0300

    feat: task solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    str = String(Math.abs(n));
+    newN = ''
+    for (i = str.length-1; i >= 0; i--){
+        newN += str[i];
+    }
+    return +newN;
 }
+
+console.log(module.exports(-538));
